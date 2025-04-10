8791a9627f2941c9cd5cb0313537e5f614eff3e4
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-04-10 09:31:47 +0800

    Added initial program flow/instruction

---
+++ source_code/quiz_creator_program.py
@@ -0,0 +1,7 @@
+# Create a program that asks user for a question, then ask for 4 possible answer and the correct answer.
+# Write the collected data to a text file.
+# Ask another question until the user chose to exit.
+
+# (Optional) Install colorama in your local device for text customization ('pip install colorama')
+
+# Import dependencies
